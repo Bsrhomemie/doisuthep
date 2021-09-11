ebb2d6772d5ce9ca18353c24cd6b87a1419065b2
Homemie_Bsr <HMhomemie@gmail.com>
2021-09-11 21:19:36 +0700

    UI

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -68,18 +68,15 @@ class ContentController extends Controller
         $data_post->post_name_en = $data['post_name_en'];
         $data_post->content_en = $data['content_en'];
         $data_post->created_at = $data['created_at'];
-        $data_post->post_type = 1;//isset($type_list[$data['post_type']])? $type_list[$data['post_type']] : '';
-        
-        var_dump($data_post);
-        die();
-        // if($request->file()) {
-        //     $fileName = time().'_'.$request->file('picture')->getClientOriginalName();
-        //     $filePath = $request->file('picture')->storeAs('uploads', $fileName, 'public');
-        //     $data_post->picture = '/storage/' . $filePath;
-        // }
+        $data_post->post_type = isset($type_list[$data['post_type']])? $type_list[$data['post_type']] : '';
+
+        if($request->file()) {
+            $fileName = time().'_'.$request->file('picture')->getClientOriginalName();
+            $filePath = $request->file('picture')->storeAs('uploads', $fileName, 'public');
+            $data_post->picture = '/storage/' . $filePath;
+        }
        
-        // $data_post->save();
-        
+        $data_post->save();
         return redirect('/admin/content/'.$data['post_type'])->with('status',"Insert successfully");
        
     }
