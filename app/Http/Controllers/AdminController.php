f43c1b281dc222704b645800a459d5a75bbbe24e
Homemie_Bsr <HMhomemie@gmail.com>
2021-09-11 23:47:35 +0700

    paginate

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -105,10 +105,10 @@ class AdminController extends Controller
 		$type_id = isset($type_list[$type])? $type_list[$type]['id'] : '' ;
 		$todo = [];
 	
-		$content = Post::where('post_type', $type_id)->get();
+		$content = Post::where('post_type', $type_id)->get()->first()->paginate(5);
 
 		
-		return view('admin.content', compact('type', 'type_text', 'content'));
+		return view('admin.content', compact('type', 'type_text', 'content'))->with('i', (request()->input('page', 1)-1) * 5);
 	}
 
 	public function listProtuct(Request $request)
