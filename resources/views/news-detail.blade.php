0e3d58dba723c6aee456e4e5dfaba02ab2b7b782
Homemie_Bsr <HMhomemie@gmail.com>
2021-09-19 18:33:39 +0700

    content_th

--- resources/views/news-detail.blade.php
+++ resources/views/news-detail.blade.php
@@ -14,6 +14,16 @@
           </p>
         </h3>
       </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="img-16by9 holder " >
+            <img src="{{URL::asset('images/cover3.jpg')}}" class="img-responsive image-preview" >
+          </div>
+        </div>
+        <div class="col-md-6">
+          {{$content->content_th}}
+        </div>  
+      </div>
       <div id="fb-root"></div>
       <script async defer crossorigin="anonymous" src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v11.0" nonce="UHtkn8Nw"></script>
       <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">แชร์</a></div>
