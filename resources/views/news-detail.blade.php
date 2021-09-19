8d76eb929dc457296d7ae9691b29df3ddb36b7fa
Homemie_Bsr <HMhomemie@gmail.com>
2021-09-19 18:50:47 +0700

    content_th

--- resources/views/news-detail.blade.php
+++ resources/views/news-detail.blade.php
@@ -28,12 +28,11 @@
             <script async defer crossorigin="anonymous" src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v11.0" nonce="UHtkn8Nw"></script>
             <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">แชร์</a></div>
           </div>
-
         </div>
-        <div class="col-12 mt-3">
+        <div class="col-12 mt-4">
           <div class="text-center">
-            <a href="#" onclick="window.history.back()"><i class="fa fa-arrow-left"></i> ย้อนกลับ</a> |
-            <a href="{{url('/')}}"><i class="fa fa-home"></i> กลับหน้าหลัก</a>
+            <a href="#" class="text-link" onclick="window.history.back()"><i class="fa fa-arrow-left"></i> ย้อนกลับ</a> |
+            <a href="{{url('/')}}" class="text-link"><i class="fa fa-home"></i> กลับหน้าหลัก</a>
           </div>
         </div>  
       </div>
