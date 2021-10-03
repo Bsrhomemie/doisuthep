95eeec1f549c78365c51e9769dbbe7190357c199
Homemie_Bsr <HMhomemie@gmail.com>
2021-10-03 16:48:14 +0700

    topic

--- resources/views/suthep.blade.php
+++ resources/views/suthep.blade.php
@@ -21,7 +21,7 @@
         <div class="col-12">
           <p class="mb-3">{!! $topic[__('message.suthep_detail')] !!}</p>
         </div>
-        @if(!isnull($topic['list'])) 
+        @if(count($topic['list'] > 0) 
         @foreach($topic['list'] as $list) 
           <div class="col-md-4">
             <div class="card card-box mb-3">
