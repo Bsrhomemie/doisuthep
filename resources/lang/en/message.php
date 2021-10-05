7fe9779d4c344af46f9c1caa24cbe2a72175d454
Homemie_Bsr <HMhomemie@gmail.com>
2021-10-06 00:57:56 +0700

    message

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -33,13 +33,38 @@
     'more' => 'More.',
     'detail' => 'Detail.',
     'map' => 'Map',
-    'organization_name' => 'ศูนย์ธรรมชาติวิทยาดอยสุเทพเฉลิมพระเกียรติฯ คณะวิทยาศาสตร์ มหาวิทยาลัยเชียงใหม่',
-    'organization_adress' => '239 ถนนห้วยแก้ว ตำบลสุเทพ อำเภอเมือง จังหวัดเชียงใหม่ 50200',
-    'organization_email' => 'อีเมล: doisuthep.center@cmu.ac.th ',
-    'organization_tel' => 'โทรศัพท์: 053-941451-3 / 084-6116345',
+    'organization_name' => 'Doi Suthep Nature Center In Honor of His Majesty the King. Faculty of Science CMU',
+    'organization_adress' => '239 Huay Kaew Road ,Suthep,  Mueang Chiang Mai District, Chiang Mai 50200',
+    'organization_email' => 'E-mail: doisuthep.center@cmu.ac.th ',
+    'organization_tel' => 'Tel: 053-941451-3 / 084-6116345',
     'suthep_detail' => 'detail_en',
     'lang_system' => 'en',
-    'no_data' => 'No data',
+    'no_data' => 'No data', 
+    'get_to_know_us' => 'Get To Know Us',
+    'our_object' => 'Our Objective ',
+    'get_to_know_us_1' => '1. To celebrate the 50th  anniversary of King Bhumibol Adulyadej Rama 9’s Reign and to congratulate the king on living to 72 years old in the year of 2542 B.E.',
+    'get_to_know_us_2' => '2. To create an event showing the stories of Doi Suthep regarding the environment, the ecosystem, history culture, and beliefs of the community for tourists, the normal population, researchers, and students before going to visit Doi Suthep. This is a big milestone to create efficient traveling and thoughtful tourists.',
+    'get_to_know_us_3' => '3. To be a coordination center between researchers and other facilities about tourism and the preservation of Doi Suthep to be the source of 
+    knowledge and provide information about Doi Suthep. ',
+    'vision_description' => '“The community sees the beauty <br> and the connection with nature.”',
+    'mission_description1' => 'Create an activity that promotes learning about nature.',
+    'mission_description2' => 'Provide natural knowledge about Doi Suthep.',
+    'mission_description3' => 'Create a community and a space to exchange knowledge to create an eco-friendly environment.',
+    'plants' => 'Plants',
+    'plants_description' => 'Have shapes that look like the shape of the mountains, as a symbol of nature in this area.',
+    'pagoda' => 'Phra That Doi Suthep <br> ( Pagoda on Doi Suthep )',
+    'pagoda_description' => 'It is placed in the center of the symbol, it is believed to be the center of the mind of the Chiangmai People.',
+    'staircase' => 'The Staircase',
+    'staircase_description' => 'Placed at the bottom, it means letting everybody understand the environment and Doi Suthep.',
+    'nong_doi' => 'Nong Doi', 
+    'nong_doi_description' => 'The bee of Doi Suthep which is the Habropoda sutepens  <br> <span class="font-14px"> is symbolled as the savior of Doi Suthep forest. </span>', 
+    'nong_ton_nam' => 'Nong Ton Nam ', 
+    'nong_ton_nam_description' => 'A Crab called  Doimon Doi <br> <span class="font-14px"> Suthep living in the waterfalls of Doi Suthep, symbolizing the restoration of Doi Suthep’s forests. </span>',
+    'nong_guy' => 'Nong Guy', 
+    'nong_guy_description' => 'The wild gecko of Doi Suthep <br> <span class="font-14px"> is called Cyrtodactylus Doi Suthep, the symbol of studying Doi Suthep’s nature. </span>', 
+    'doi_suthep_description' => 'Doi Suthep – Pui Natural Park is counted as the 24th natural park in Thailand. It was made official on the 14th of April in 2524 B.E. it covers 100,662.50 Rai or 161.06 square kilometers of land. It covers Amphur Muang, Han Dong, Mae Lim and, Mae Tang sub-districts of Chiang Mai. The highest elevation point is in Doi Pui which is 1,685 meters above sea level. This Natural Park has an abundant forest despite being only 6 Kilometers away from the city.'
+
+
   ];
   
 ?>
