37330459fe5bfb02d9920444de905b41227002e1
Homemie_Bsr <HMhomemie@gmail.com>
2021-11-08 22:54:11 +0700

    table product

---
+++ database/migrations/2021_11_08_154918_create_product_tbl.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTbl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_tbl', function (Blueprint $table) {
+            $table->id();
+            $table->text('name_th');
+            $table->text('name_en');
+            $table->float('price');
+            $table->int('status');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_tbl');
+    }
+}
