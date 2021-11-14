0229b568e3a02a06c5958a36e9ff76cd3799901f
Homemie_Bsr <HMhomemie@gmail.com>
2021-11-14 17:48:12 +0700

    editProduct

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::get('/admin/product/', [AdminController::Class,'listProtuct']);
 Route::get('/product/form/', [ProductController::Class,'viewProtuct']);
 Route::post('/product/add/', [ProductController::Class,'addProtuct']);
 Route::get('/product/form_edit/{id}', [ProductController::Class,'viewProtuct']);
-Route::post('product/edit/', [ProductController::Class,'editProtuct']);
+Route::post('product/edit/', [ProductController::Class,'editProduct']);
 
 // Route::get('/admin/vedio', [AdminController::Class,'listVedio']);
 Route::get('/admin/work', [AdminController::Class,'listWork']);
