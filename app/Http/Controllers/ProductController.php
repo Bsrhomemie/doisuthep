1508bd714e124ec88ec413fb8f9b130fc4f494ff
Homemie_Bsr <HMhomemie@gmail.com>
2021-11-14 15:50:53 +0700

    $

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,7 +28,8 @@ class ProductController extends Controller
 
     public function addProtuct(Request $request)
     {
-
+        dd($request);
+        die();
         $data = $request->input();
         $data_product = new Product;
         $data_product->name_th = $data['name_th'];
