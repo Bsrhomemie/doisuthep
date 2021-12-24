c8a7ed0e7d793ab607e2ebc955df72b96b49d4cc
Bsrhomemie <87844831+Bsrhomemie@users.noreply.github.com>
2021-12-25 03:39:42 +0700

    x

--- ford-canteen/script.js
+++ ford-canteen/script.js
@@ -1,3 +1,3 @@
 var imported = document.createElement('script');
-imported.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js';
+imported.src = 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js';
 document.body.appendChild(imported);
